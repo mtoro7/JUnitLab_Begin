647627d0033e9b58eb9051e4c82b70271d3e704c
Marco Toro <marcotoro97@hotmail.com>
2021-03-08 17:11:16 -0800

    changed a bunch of stuff, finally done with HW 4

--- src/main/java/csc131/junit/GiftCardTest.java
+++ src/main/java/csc131/junit/GiftCardTest.java
@@ -38,4 +38,73 @@ class GiftCardTest {
 		assertEquals("getBalance()", balance, card.getBalance(), 0.001);
 	}
 	
+	@Test
+	public void deduct_RemainingBalance()
+	{
+		double balance;
+		GiftCard card;
+		int issuingStore;
+		String expected;
+		
+		issuingStore = 1337;
+		balance = 100.00;
+		card = new GiftCard(issuingStore, balance);
+		
+		expected = "Remaining Balance: " + String.format("%6.2f", 90.00);
+		assertEquals("deduct(10.00)", expected, card.deduct(10.0));
+		
+	}
+	
+	@Test
+	public void deduct_InvalidTransaction()
+	{
+		double balance;
+		GiftCard card;
+		int issuingStore;
+		String expected;
+		
+		issuingStore = 1337;
+		balance = 100.00;
+		card = new GiftCard(issuingStore, balance);
+		
+		expected = "Invalid Transaction";
+		assertEquals("deduct 10.00", expected, card.deduct(-10.0));
+	}
+	
+	@Test
+	public void deduct_AmountDue()
+	{
+		double balance;
+		GiftCard card;
+		int issuingStore;
+		String expected;
+		
+		issuingStore = 1337;
+		balance = 100.00;
+		card = new GiftCard(issuingStore, balance);
+		
+		expected = "Amount Due: " + String.format("%6.2f", 10.00);
+		assertEquals("deduct 110.00", expected, card.deduct(110.0));
+	}
+	
+	
+	@Test
+	public void constructor_IncorrectBallance()
+	{
+		assertThrows(IllegalArgumentException.class, () -> {new GiftCard(1,-100.00);});
+	}
+	
+	
+	@Test
+	public void constructor_IncorrectID_Low() 
+	{
+		assertThrows(IllegalArgumentException.class, () -> {new GiftCard(-1,100.00);});
+	}
+	
+	@Test
+	public void constructor_IncorrectID_High() 
+	{
+		assertThrows(IllegalArgumentException.class, () -> {new GiftCard(100000,100.00);});
+	}
+	
 }
